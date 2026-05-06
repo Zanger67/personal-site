f98b11ec3079995afc98c1e2937e753ff3cda91e
Anthony Zang <anthonyzang101@gmail.com>
2026-05-06 16:13:23 -0400

    add sandwich redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://zang.dev',
+  redirects: {
+    '/s': 'https://rotatingsandwiches.com',
+    '/sand': 'https://rotatingsandwiches.com',
+    '/sandwich': 'https://rotatingsandwiches.com',
+    '/sandwiches': 'https://rotatingsandwiches.com',
+  },
 });
